3d3f89ba22e20e8e00ca131637478941d36a8ed3
Brandon <Brandonstowe71@gmail.com>
2021-09-22 14:55:58 -0500

    Update profile-view.jsx

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import {Button, Form, Row} from 'react-bootstrap';
+import {Button,Card, CardDeck, Form, Row} from 'react-bootstrap';
 
 export class ProfileView extends React.Component {
   constructor() {
@@ -10,7 +10,7 @@ export class ProfileView extends React.Component {
       password: null,
       email: null,
       birthday: null,
-      favoriteMovies: null,
+      favoriteMovies: [],
       validated: null
     }
   }
@@ -45,7 +45,7 @@ export class ProfileView extends React.Component {
     const token = localStorage.getItem('token');
     const username = localStorage.getItem('user');
 
-    axios.delete(`https://myflixdbcfv3.herokuapp.com/user/${username}/removeFavorite/${movie.id}`, {
+    axios.delete(`https://myflixdbcfv3.herokuapp.com/user/${username}/removeFavorite/${movie._id}`, {
       headers: {Authorization: `Bearer ${token}` },
     })
     .then(() => {
@@ -76,17 +76,17 @@ export class ProfileView extends React.Component {
     const username = localStorage.getItem('user');
 
     axios.put(`https://myflixdbcfv3.herokuapp.com/user/userinfoChange/${username}`, 
-      {username: newUsername ? newUsername : this.state.username}, 
-      {password: newPassword ? newPassword : this.state.password},
-      {email: newEmail ? newEmail : this.state.email},
-      {birthday: newBirthday ? newBirthday : this.state.birthday},
+      {username: newUsername ? newUsername : this.state.username, 
+        password: newPassword ? newPassword : this.state.password, 
+        email: newEmail ? newEmail : this.state.email, 
+        birthday: newBirthday ? newBirthday : this.state.birthday}, 
       {headers: {Authorization: `Bearer ${token}` }}
       )
     .then((response) => {
       alert('Username has been updated');
       this.setState({
         username: response.data.username,
-        password: response.dtat.password,
+        password: response.data.password,
         email: response.data.email,
         birthday: response.data.birthday
       });
@@ -118,7 +118,7 @@ export class ProfileView extends React.Component {
     e.preventDefault();
 
     const token = localStorage.getItem('token');
-    const user = localStorage.getItem('user');
+    const username = localStorage.getItem('user');
 
     axios.delete(`https://myflixdbcfv3.herokuapp.com/user/unregister/${username}`, {
       headers: {Authorization: `Bearer ${token}` },
@@ -140,27 +140,66 @@ export class ProfileView extends React.Component {
     const {favoriteMovies, validated} = this.state;
 
     return (
-      <Row className="prfile-view">
-        <h1>Update your Account info</h1><br />
-          <Form noVaildate validated={validated} className="update-form" onSubmit={(e) => this.handleUpdate(e, this.username, this.password, this.email, this.birthday)}>
-            <Form.Group controlId="formUsername">
-              <Form.Label className="form-label">Username: </Form.Label>
-              <Form.Control type="text" placeholder="Change Username" onChange={(e) => this.setUsername(e.target.value)} />
-            </Form.Group>
-            <Form.Group controlId="formPassword">
-              <Form.Label className="form-label">Password: </Form.Label>
-              <Form.Control type="text" placeholder="Change Password" onChange={(e) => this.setPassword(e.target.value)} />
-            </Form.Group>
-            <Form.Group controlId="formEmail">
-              <Form.Label className="form-label">Email: </Form.Label>
-              <Form.Control type="text" placeholder="Change Email" onChange={(e) => this.setEmail(e.target.value)} />
-            </Form.Group>
-            <Form.Group controlId="formBirthday">
-              <Form.Label className="form-label">Birthday: </Form.Label>
-              <Form.Control type="text" placeholder="Change Birthday" onChange={(e) => this.setBirthday(e.target.value)} />
-            </Form.Group>
-            <Button varient="primary" type="submit">Update</Button>
-          </Form>
+      <Row className="prfile-view justify-content-md-center">
+        <Card className="profile-card">
+          <h2>Your favorite movies</h2>
+          <Card.Body>
+            {favoriteMovies.length === 0 && <div className="text-center">Empty</div>}
+            <div className="favorite-movies">
+              {favoriteMovies.length > 0 && 
+              favoriteMovies.map((movie) => {
+                if (movie._id === favoriteMovies.find((favMovie) => favmovies === movie._id)) {
+                  return (
+                    <CardDeck className="movie-card-deck">
+                      <Card className="favorite-item card-content" style={{ width: '16rem' }} key={movie._id}>
+                        <Card.Img style={{width: '18rem'}} className="movieCard" varient="top" src={movie.ImageUrl} />
+                        <Card.Body>
+                          <Card.Title className="movie-card-title">{movie.title}</Card.Title>
+                          <Button size="sm" className="profile-button remove-favorite" varient="primary" value={movie._id} onClick={(e) => this.removeFavoriteMovie(e, movie)}>
+                            Remove 
+                          </Button>
+                        </Card.Body> 
+                      </Card>
+                    </CardDeck>
+                  )
+                }
+              })}
+            </div>
+          </Card.Body>
+        
+          <Card.Body>
+            <Form noVaildate validated={validated} className="update-form" onSubmit={(e) => this.handleUpdate(e, this.username, this.password, this.email, this.birthday)}>
+              <h2>Update your user info</h2>
+              <Form.Group controlId="formUsername">
+                <Form.Label className="form-label">Username: </Form.Label>
+                <Form.Control type="text" placeholder="Change Username" onChange={(e) => this.setUsername(e.target.value)} />
+              </Form.Group>
+
+              <Form.Group controlId="formPassword">
+                <Form.Label className="form-label">Password: </Form.Label>
+                <Form.Control type="text" placeholder="Change Password" onChange={(e) => this.setPassword(e.target.value)} />
+              </Form.Group>
+
+              <Form.Group controlId="formEmail">
+                <Form.Label className="form-label">Email: </Form.Label>
+                <Form.Control type="text" placeholder="Change Email" onChange={(e) => this.setEmail(e.target.value)} />
+              </Form.Group>
+
+              <Form.Group controlId="formBirthday">
+                <Form.Label className="form-label">Birthday: </Form.Label>
+                <Form.Control type="date" onChange={(e) => this.setBirthday(e.target.value)} />
+              </Form.Group>
+
+              <Button varient="primary" type="submit">Update</Button>
+
+              <h2>Delete your account</h2>
+              <Card.Body>
+                <Button varient="primary" type="submit" onClick={(e) => this.handleDeregister(e)}>Delete Account</Button>
+                <Button onClick={() => { onBackClick(null); }}>Back</Button>
+              </Card.Body>
+            </Form>
+          </Card.Body>
+        </Card>
       </Row>
     );
   }

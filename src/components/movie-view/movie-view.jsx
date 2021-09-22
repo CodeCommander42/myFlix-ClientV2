44c22b13f374e80d12a2af8cf1f72449920d8f0d
Brandon <Brandonstowe71@gmail.com>
2021-09-22 17:56:36 -0500

    add to favorites function in progress

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,8 +1,24 @@
+import axios from 'axios';
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import {Link} from 'react-router-dom';
 
 export class MovieView extends React.Component {
+
+  addFavorite() {
+    const token = localStorage.getItem('token');
+    const username = localStorage.getItem('user');
+
+    axios.post(`https://myflixdbcfv3.herokuapp.com/user/${username}/addFavorite/${movie._id}`, {}, {
+      headers: {Authorization: `Bearer ${token}`}
+    })
+    .then(response => {
+      alert('Added to favorites list');
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  };
 
   render() {
     const { movie, onBackClick } = this.props;
@@ -32,6 +48,9 @@ export class MovieView extends React.Component {
             <Button variant="link">{movie.genre.name}</Button>
           </Link>  
         </div>
+        <Button varient="primary" className="favorite-button" value={movie._id} onClick={(e) => this.addFavorite(e, movie)}>
+          Add to Favorites
+        </Button>
         <button onClick={() => { onBackClick(null); }}>Back</button>
 
       </div>

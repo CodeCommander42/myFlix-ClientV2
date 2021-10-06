99f08f2040535301cc2b18cb05f51f1560681d31
Brandon <Brandonstowe71@gmail.com>
2021-10-06 12:59:47 -0500

    update

--- src/components/movie-list/movie-list.jsx
+++ src/components/movie-list/movie-list.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Col from 'react-bootsrap/Col';
+import Col from 'react-bootstrap/Col';
 import {connect} from 'react-redux';
 
 import VisibilityFilterInput from '../visibility-filter-input/visibility-filter-input';
